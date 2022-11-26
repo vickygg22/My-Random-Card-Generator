fd3d267ba53a724d901afbab0291b9b87f2ea91a
Victoria <victoria1998castellar@hotmail.com>
2022-11-26 09:22:40 +0000

    final version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".cardValue").innerHTML = generateRandomValue();
+  function generateRandomValue() {
+    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let randomValues = Math.floor(Math.random() * values.length);
+    return values[randomValues];
+  }
+  let mainDiv = document.querySelector(".card");
+  mainDiv.classList.add(generateRandomSuit());
+  function generateRandomSuit() {
+    let suits = ["heart", "diamond", "spade", "club"];
+    let randomSuits = Math.floor(Math.random() * suits.length);
+    return suits[randomSuits];
+  }
 };
